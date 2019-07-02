e8628bb6554e33136c4ad1530e487a1310ce93b6
Mike Kasprzak <mikekasprzak@gmail.com>
2019-07-02 13:37:38 -0400

    initial commit, it compiles on linux

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <Processing.NDI.Lib.h>
+
+int main(int argc, char* argv[]) {
+	NDIlib_find_instance_t nf = NDIlib_find_create_v2(nullptr);
+	
+	if (!nf) {
+		printf("Unable ta create finder\n");
+		
+		return -1;
+	}
+	
+	
+	while (true) {
+		NDIlib_find_wait_for_sources(nf, 1000);
+		
+		uint32_t sourcesCount;
+		const NDIlib_source_t* pSources = NDIlib_find_get_current_sources(nf, &sourcesCount);
+		
+		printf("Found %i sources\n", sourcesCount);
+	};
+
+
+	NDIlib_find_destroy(nf);
+
+	return 0;
+}
