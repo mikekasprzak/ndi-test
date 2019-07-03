b904ae28a72557326033f25b2187fc372565e22c
Mike Kasprzak <mikekasprzak@gmail.com>
2019-07-03 13:41:24 -0400

    early exit and enumerating available connections

--- main.cpp
+++ main.cpp
@@ -12,14 +12,19 @@ int main(int argc, char* argv[]) {
 	}
 	
 	
-	while (true) {
+	for (int idx = 0; idx < 20; ++idx) {
 		NDIlib_find_wait_for_sources(nf, 1000);
 		
 		uint32_t sourcesCount;
 		const NDIlib_source_t* pSources = NDIlib_find_get_current_sources(nf, &sourcesCount);
 		
 		printf("Found %i sources\n", sourcesCount);
-	};
+		
+		for (int s = 0; s < sourcesCount; ++s) {
+			
+			printf("* %s: %s\n", pSources[s].p_ndi_name, pSources[s].p_url_address);
+		}
+	}
 
 
 	NDIlib_find_destroy(nf);
